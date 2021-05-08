c6bcde0ff2bfcc8d8bfd9902ac4e1e6f838521dd
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-09 00:45:45 +0200

    Adding Application Config Class. closes #1

---
+++ src/main/java/com/test/hplus/ApplicationConfig.java
@@ -0,0 +1,35 @@
+package com.test.hplus;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * @author Mohammed Amr
+ * @created 09/05/2021 - 00:38
+ * @project hplus-app
+ */
+
+@Configuration
+@ComponentScan(basePackages = "com.test.hplus")
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("css/**", "images/**")
+                .addResourceLocations("classpath:/static/css/", "classpath:/static/images/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver jspViewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        viewResolver.setViewClass(JstlView.class);
+        return viewResolver;
+    }
+}
