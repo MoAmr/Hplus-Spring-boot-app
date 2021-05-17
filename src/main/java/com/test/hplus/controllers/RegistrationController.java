a76bc76870d02567c3a9071c475dd368b75da4a8
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-17 02:13:46 +0200

    Adding validations to user bean. closes #11

--- src/main/java/com/test/hplus/controllers/RegistrationController.java
+++ src/main/java/com/test/hplus/controllers/RegistrationController.java
@@ -5,8 +5,11 @@ import com.test.hplus.repositrory.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 /**
  * @author Mohammed Amr
@@ -21,8 +24,12 @@ public class RegistrationController {
     private UserRepository userRepository;
 
     @PostMapping("/registeruser")
-    public String registerUser(@ModelAttribute("newuser")User user, Model model) {
+    public String registerUser(@Valid @ModelAttribute("newuser")User user, BindingResult result, Model model) {
         System.out.println("In registration controller!");
+
+        if (result.hasErrors()) {
+            return "register";
+        }
         userRepository.save(user);
         model.addAttribute("dataSaved", "User registered successfully");
         return "login";
