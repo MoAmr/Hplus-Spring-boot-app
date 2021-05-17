6bbc7fdd3e486994d998ea1f3f88e0eceeb8f91d
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-17 18:51:41 +0200

    Localizing error messages. closes #13

--- src/main/java/com/test/hplus/beans/User.java
+++ src/main/java/com/test/hplus/beans/User.java
@@ -19,7 +19,7 @@ public class User {
     @Id
     private int id;
 
-    @Size(min = 6, message = "Username cannot be less than 6 characters!")
+    @Size(min = 6, message = "{username.cannot.be.less.than.six.characters}")
     private String username;
 
     @Pattern(regexp = "((?=.*[A-Z]).{6,10})", message = "Password must have one uppercase, one lowercase, and should be between 6 and 10 characters!")
