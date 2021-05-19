3622db078b8df37be2282d094fc0bd2a32aebc1b
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-19 02:03:09 +0200

    Async Configuration. closes #21

--- src/main/java/com/test/hplus/config/ApplicationConfig.java
+++ src/main/java/com/test/hplus/config/ApplicationConfig.java
@@ -4,7 +4,10 @@ import com.test.hplus.convertors.StringToEnumConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -38,5 +41,18 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addFormatters(FormatterRegistry registry) {
         registry.addConverter(new StringToEnumConverter());
+    }
+
+    @Override
+    protected void configureAsyncSupport(AsyncSupportConfigurer configurer) {
+        configurer.setDefaultTimeout(5000);
+        configurer.setTaskExecutor(mvcTaskExecutor());
+    }
+
+    @Bean
+    public AsyncTaskExecutor mvcTaskExecutor() {
+        ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setThreadNamePrefix("hplusapp-thread-");
+        return threadPoolTaskExecutor;
     }
 }
