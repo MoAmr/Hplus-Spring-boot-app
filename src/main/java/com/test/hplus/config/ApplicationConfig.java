8b6307bfdf64aa99ae8f3bb7368c56729d53fb77
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-21 03:21:51 +0200

    CookieLocaleResolver and interceptor for l10n and i18n. closes #36

--- src/main/java/com/test/hplus/config/ApplicationConfig.java
+++ src/main/java/com/test/hplus/config/ApplicationConfig.java
@@ -10,17 +10,22 @@ import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.task.AsyncTaskExecutor;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.ThemeResolver;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.theme.CookieThemeResolver;
 import org.springframework.web.servlet.theme.ThemeChangeInterceptor;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.ResourceBundleViewResolver;
 import org.springframework.web.servlet.view.XmlViewResolver;
+
+import java.util.Locale;
 
 /**
  * @author Mohammed Amr
@@ -84,7 +89,8 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
     @Override
     protected void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new LoggingInterceptor()).addPathPatterns("/*");
-        registry.addInterceptor(new ThemeChangeInterceptor());
+        registry.addInterceptor(new ThemeChangeInterceptor()); //theme
+        registry.addInterceptor(new LocaleChangeInterceptor()); //locale
     }
 
     @Bean
@@ -95,4 +101,11 @@ public class ApplicationConfig extends WebMvcConfigurationSupport {
         return cookieThemeResolver;
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+        cookieLocaleResolver.setDefaultLocale(Locale.US);
+        cookieLocaleResolver.setCookieName("locale");
+        return cookieLocaleResolver;
+    }
 }
