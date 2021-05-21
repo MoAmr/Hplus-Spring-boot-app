7a9a514bad62c38a0d0458b8a1e448499fd8d147
Mohammed Amr <haveagoodday.mido@gmail.com>
2021-05-21 03:40:50 +0200

    Use @RequestController and @ResponseEntity. closes #38

--- src/main/java/com/test/hplus/restcontroller/ProductsRestController.java
+++ src/main/java/com/test/hplus/restcontroller/ProductsRestController.java
@@ -3,9 +3,13 @@ package com.test.hplus.restcontroller;
 import com.test.hplus.beans.Product;
 import com.test.hplus.repositrory.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,18 +20,25 @@ import java.util.List;
  * @project hplus-app
  */
 
-@Controller
+//@Controller
+@RestController
 public class ProductsRestController {
 
     @Autowired
     private ProductRepository productRepository;
 
-    @GetMapping("/hplus/rest/products")
+    /*@GetMapping("/hplus/rest/products")
     @ResponseBody
     public List<Product> getProducts() {
         // call product repo
         List<Product> products = new ArrayList<>();
         productRepository.findAll().forEach(product -> products.add(product));
         return products;
+    }*/
+
+    @GetMapping("/hplus/rest/products")
+    public ResponseEntity getProductsByResponseParam(@RequestParam("name") String name) {
+        List<Product> products = productRepository.searchByName(name);
+        return new ResponseEntity<>(products, HttpStatus.OK);
     }
 }
